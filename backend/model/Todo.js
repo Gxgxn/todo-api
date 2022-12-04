ecfd975ef9447ec05a065c29492377b8d2246c20
Gxgxn <gdsinghb99@gmail.com>
2022-12-03 20:27:46 -0400

    updated todo modal for auth

--- backend/model/Todo.js
+++ backend/model/Todo.js
@@ -3,12 +3,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema({
+  userId: {
+    type: String,
+    require: [true, "userID is required"],
+  },
   title: {
     type: String,
     required: [true, "title is required"],
-    timestamps: true,
+    maxlength: [40, "title must be under 40 characters"],
+    trim: true,
   },
-  tasks: [String],
+  tasks: [
+    {
+      task: { type: String, trim: true },
+      isDone: { type: Boolean, default: false },
+      timestamps: true,
+    },
+  ],
 });
 
 const TodoModel = mongoose.model("Todo", TodoSchema);
